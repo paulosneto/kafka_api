af68ee01a9edd7637ce77c3ca4741ad779edd48a
paulo_sneto <paulo_sneto@yahoo.com>
2024-05-23 15:38:47 -0300

    feat: created standard test file ConsumerApiApplicationTests.java

---
+++ consumer-api/src/test/java/com/api/consumer/ConsumerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
