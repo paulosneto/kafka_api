f398effd825501e034f211cbaade782329fb12f5
paulo_sneto <paulo_sneto@yahoo.com>
2024-05-23 15:37:59 -0300

    feat: created standard file ConsumerApiApplication.java

---
+++ consumer-api/src/main/java/com/api/consumer/ConsumerApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApiApplication.class, args);
+	}
+
+}
